f6192fb95891c0eb111d626739c49f87367ebaf1
gitmichelhub <michel.lasse.werner@gmail.com>
2025-08-08 17:51:41 +0200

    default DE

--- src/hooks/use-language.tsx
+++ src/hooks/use-language.tsx
@@ -11,7 +11,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('de');
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
